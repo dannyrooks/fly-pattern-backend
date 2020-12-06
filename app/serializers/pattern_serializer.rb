a520d951cc71af3089e55b29cfd2fb90cbf25441
Daniel Rooker <dannyrooks@gmail.com>
2020-12-06 16:41:46 -0500

    serializer revert

--- app/serializers/pattern_serializer.rb
+++ app/serializers/pattern_serializer.rb
@@ -1,4 +1,14 @@
 class PatternSerializer < ActiveModel::Serializer
   attributes :id, :name, :category, :comment
   has_many :materials
+
+  # attribute :material do |pattern|
+  #   mat = pattern.material
+  #     {
+  #       id: mat.id,
+  #       name: mat.name,
+  #       description: mat.description
+  #     }
+
+  # end
 end
